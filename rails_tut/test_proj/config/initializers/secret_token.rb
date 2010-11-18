8dfeed8c1dc53cd6eebd7413553d19dfaba9dc3e
Tristan <tristan@dynamicclarity.com>
2010-11-18 14:26:12 +1100

    message

---
+++ rails_tut/test_proj/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestProj::Application.config.secret_token = 'b249347db318aae564974ca3005b693d8ed57da4d601036ed089eb36c329b937f63c1a21b0e57825570823541ea64a6c98060ade7220caca29a3853bd3b447c9'
